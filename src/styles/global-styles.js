c4cbd8e16d26df098bd382d557ad487f6dfde934
Juan Luis Garcia Garcia <juanluisggarcia@gmail.com>
2024-03-19 22:52:06 +0100

    Initial commit

---
+++ src/styles/global-styles.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  *,
+  *::after,
+  *::before{
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  img{
+    display: block;
+    max-width: 100%;
+  }
+  
+  body{
+    font-family: sans-serif;
+  }
+
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul{
+    list-style: none;
+    margin-top: 0;
+    margin-bottom: 0;
+    padding-left: 0;
+  }
+`;
